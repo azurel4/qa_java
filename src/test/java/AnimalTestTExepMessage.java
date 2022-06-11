196335cab05181ae4758bc7ab7189afde3da7608
Larisa Guliyan <4417644@gmail.com>
2022-06-12 00:57:24 +0300

    final commit2

--- src/test/java/AnimalTestTExepMessage.java
+++ src/test/java/AnimalTestTExepMessage.java
@@ -2,6 +2,8 @@ import com.example.Animal;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 
 public class AnimalTestTExepMessage {
@@ -10,7 +12,7 @@ public class AnimalTestTExepMessage {
     public void testGetFoodException(){
         Animal animal = new Animal();
         try {
-            animal.getFood("gbgjhbjh");
+            animal.getFood("прапопрр");
         }catch (Exception thrown){
             String expected = "Неизвестный вид животного, используйте значение Травоядное или Хищник";
             String actual = thrown.getMessage();
@@ -18,12 +20,13 @@ public class AnimalTestTExepMessage {
         }
     }
 
+
     @Test
     public void testGetFamilyAnimal(){
         Animal animal = new Animal();
         String expected = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
         String actual = animal.getFamily();
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
 }
